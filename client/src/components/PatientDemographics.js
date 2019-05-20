fb5858403a272657bc28a60322c11d29e1038c93
ryanoconnor8 <50682824+ryanoconnor8@users.noreply.github.com>
2019-05-19 21:32:23 -0400

    Update PatientDemographics.js

--- client/src/components/PatientDemographics.js
+++ client/src/components/PatientDemographics.js
@@ -3,38 +3,57 @@ import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import Search from '@material-ui/icons/Search';
 
 const PatientDemographics = () => {
     return (
         <React.Fragment>
             <List>
+                <FormControl>
+                    <InputLabel htmlFor="input-with-icon-adornment">Patient Search</InputLabel>
+                    <Input
+                        id="input-with-icon-adornment"
+                        startAdornment={
+                            <InputAdornment position="start">
+                                <Search />
+                            </InputAdornment>
+                        }
+                    />
+                </FormControl>
                 <ListItem>
-                    <ListItemText primary="Age :" />
+                    <ListItemText secondary="This information was last updated 05/19/2019 at 2:47am from the CDW" />
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Sex :" />
+                    <ListItemText primary="Age : 87" />
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Weight :" />
+                    <ListItemText primary="Sex : M" />
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Height :" />
+                    <ListItemText primary="Weight : 5'8" />
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="BMI :" />
+                    <ListItemText primary="Height : 210" />
+                </ListItem>
+                <ListItem>
+                    <ListItemText primary="BMI : 25" />
                 </ListItem>
                 <Divider />
                 <ListItem>
-                    <ListItemText primary="Drug :" />
+                    <ListItemText primary="Drug : Rivaroxaban (Xarelto)" />
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Indication :" />
+                    <ListItemText primary="Indication : Reduces the risk of stroke in nonvalvular atrial fibrillation" />
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Inpatient Dose :" />
+                    <ListItemText primary="Inpatient Dose : 20 mg taken once daily" />
                 </ListItem>
                 <ListItem>
-                    <ListItemText primary="Outpatient Dose :" />
+                    <ListItemText primary="Outpatient Dose : 20 mg taken once daily" />
                 </ListItem>
             </List>
         </React.Fragment>
